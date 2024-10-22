8ea716bf2bb178dafe02c20fbcd2405dd952f58e
jonklop <youssefhany2073@gmail.com>
2024-10-22 19:42:22 +0300

    Project

---
+++ AR_Clothing_GUI/AR_Clothing_GUI/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace AR_Clothing_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(452, 141);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(211, 265);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(693, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 73);
+            this.button1.TabIndex = 1;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(356, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 73);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(21, 98);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(135, 36);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "T-Shirts";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(21, 156);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(141, 36);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Hoodies";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(21, 213);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(109, 36);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Pants";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton4.Location = new System.Drawing.Point(21, 271);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(209, 36);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Long Sleeves";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(270, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 50);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "AR Clothing";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(858, 503);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AR_Clothing_GUI/AR_Clothing_GUI/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Text;
+
+namespace AR_Clothing_GUI
+{
+    public partial class Form1 : Form
+    {
+        string[] tShirtImages = { "blackTshirt.PNG", "whiteTshirt.PNG","blueTshirt.PNG","greenTshirt.PNG" ,"redTshirt.PNG" };
+        string[] hoodieImages = { "blueHoodie.PNG", "yellowHoodie.PNG", "greenHoodie.PNG" };
+        string[] pantsImages = { "biegePants.PNG", "brownPants.PNG" };
+        string[] longSleeveImages = { "blackLongSleeve.PNG", "whiteLongSleeve.PNG" };
+
+        int currentColorIndex = 0;
+        string[] currentImages;
+        int tShirtColorIndex = 0;
+        int hoodieColorIndex = 0;
+        int pantsColorIndex = 0;
+        int longSleeveColorIndex = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            radioButton1.CheckedChanged += new EventHandler(radioButton_CheckedChanged);
+            radioButton2.CheckedChanged += new EventHandler(radioButton_CheckedChanged);
+            radioButton3.CheckedChanged += new EventHandler(radioButton_CheckedChanged);
+            radioButton4.CheckedChanged += new EventHandler(radioButton_CheckedChanged);
+
+            currentImages = tShirtImages;
+            DisplayCurrentImage();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+        private void SendClothingSelection(string data)
+        {
+            try
+            {   
+                // tcp client used for the server connection(sends data for the clothes)
+                using (TcpClient client = new TcpClient("127.0.0.1", 12347))
+                {
+                    NetworkStream stream = client.GetStream();
+                    byte[] message = Encoding.ASCII.GetBytes(data);
+                    stream.Write(message, 0, message.Length);
+                    stream.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error sending data: " + ex.Message);
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e) => ChangeImage(-1);
+        private void button1_Click(object sender, EventArgs e) => ChangeImage(1);
+        private void ChangeImage(int direction)
+
+        {
+
+            currentColorIndex = (currentColorIndex + direction + currentImages.Length) % currentImages.Length;
+
+            SaveCurrentColorIndex();
+
+            string selectedColor = currentImages[currentColorIndex];
+
+            string data = GetSelectedClothingType() + ";" + selectedColor;
+
+
+            DisplayCurrentImage();
+
+            SendClothingSelection(data);
+
+            SendCommandToTuiO("NEXT"); // Send a command to indicate the next item
+
+        }
+        private void SendCommandToTuiO(string command)
+
+        {
+
+            try
+
+            {
+
+                using (TcpClient client = new TcpClient("127.0.0.1", 12347)) // Change to the new port
+
+                {
+
+                    NetworkStream stream = client.GetStream();
+
+                    byte[] message = Encoding.ASCII.GetBytes(command);
+
+                    stream.Write(message, 0, message.Length);
+
+                    stream.Flush();
+
+                }
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show("Error sending command to TUIO: " + ex.Message);
+
+            }
+
+        }
+        private string GetSelectedClothingType()
+        {
+            if (radioButton1.Checked)
+                return "TShirt";
+            else if (radioButton2.Checked)
+                return "Hoodie";
+            else if (radioButton3.Checked)
+                return "Pants";
+            else if (radioButton4.Checked)
+                return "LongSleeve";
+
+            return "";
+        }
+        private void UpdateCurrentImages(RadioButton rb)
+        {
+            switch (rb.Text)
+            {
+                case "T-Shirts":
+                    currentImages = tShirtImages;
+                    currentColorIndex = tShirtColorIndex;
+                    break;
+                case "Hoodies":
+                    currentImages = hoodieImages;
+                    currentColorIndex = hoodieColorIndex;
+                    break;
+                case "Pants":
+                    currentImages = pantsImages;
+                    currentColorIndex = pantsColorIndex;
+                    break;
+                case "Long Sleeves":
+                    currentImages = longSleeveImages;
+                    currentColorIndex = longSleeveColorIndex;
+                    break;
+                default:
+                    currentImages = tShirtImages; 
+                    currentColorIndex = 0;
+                    break;
+            }
+        }
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+
+            if (rb.Checked)
+            {
+                UpdateCurrentImages(rb); 
+                string selectedColor = currentImages[currentColorIndex];
+                string data = GetSelectedClothingType() + ";" + selectedColor;
+
+                DisplayCurrentImage(); 
+                SendClothingSelection(data); 
+            }
+        }
+        private void SaveCurrentColorIndex()
+        {
+           
+            if (radioButton1.Checked)
+                tShirtColorIndex = currentColorIndex;
+            else if (radioButton2.Checked)
+                hoodieColorIndex = currentColorIndex;
+            else if (radioButton3.Checked)
+                pantsColorIndex = currentColorIndex;
+            else if (radioButton4.Checked)
+                longSleeveColorIndex = currentColorIndex;
+        }
+        private void DisplayCurrentImage()
+        {
+            if (System.IO.File.Exists(currentImages[currentColorIndex]))
+            {
+                pictureBox1.ImageLocation = currentImages[currentColorIndex];
+            }
+            else
+            {
+                MessageBox.Show("Image not found: " + currentImages[currentColorIndex]);
+            }
+        }   
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
